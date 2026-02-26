8a9fd7759422d689f4f08d644ad324f633ec2a2f
Ed <ed.gie0696@gmail.com>
2026-02-26 18:30:41 +1300

    use function and for loop to repeat string

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let final = ""
+    if (num >=0) {
+        for (let index = 0; index < num; index++) {
+            final += string;
+        }
+    }
+
+    else {
+        final = "ERROR";
+    }
+    return final;
 };
 
 // Do not edit below this line
