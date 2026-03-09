8188fb98edf11108cac572543a96be4f44284c5d
Ed <ed.gie0696@gmail.com>
2026-03-10 12:44:01 +1300

    create function that returns sum of 2 integers and all numbers in between

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(minVal, maxVal) {
+    let total = 0;
+    if ((Number.isInteger(minVal) && minVal > 0) && (Number.isInteger(maxVal) && maxVal > 0)) {
+        // swaps the arguments if the higher number is the first argument
+        if (minVal > maxVal) {
+        let temp = minVal;
+        minVal = maxVal;
+        maxVal = temp;
+        }
 
-};
+        for (let i = minVal; i <= maxVal; i++) {
+            total += i;
+        }
+        return total;
+    }
+    else {
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log(sumAll(1, 4));
