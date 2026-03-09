4f8aefc4f2e24804a7f6c462d5dd53af56dff50b
Ed <ed.gie0696@gmail.com>
2026-03-10 11:19:36 +1300

    create function that accepts multiple arguments to remove from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter((element) => !args.includes(element));
 };
+
+console.log(removeFromArray([1,2,3,4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
