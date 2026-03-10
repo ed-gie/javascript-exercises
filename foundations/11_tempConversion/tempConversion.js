01f66204f6d91f12c68f67b8e3aeefd50259e540
Ed <ed.gie0696@gmail.com>
2026-03-10 14:57:02 +1300

    create 2 functions to convert celsius and faherenheit

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const result = Number((fahrenheit - 32) * 5/9);
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const result = Number((celsius * 9/5) + 32);
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
@@ -9,3 +13,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+console.log(convertToFahrenheit(73.2));
