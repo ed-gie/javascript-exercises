4065920ab5a4992ca22a1b1df72f27c7e0767df2
Ed <ed.gie0696@gmail.com>
2026-03-28 14:02:23 +1300

    complete task and tests

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    const sequence = [1, 1];
+    if (Number(num) === 0) {
+        return 0;
+    }
+    else if (Number(num) < 0) {
+        return "OOPS";
+    }
+    for (let i = 0; i < (Number(num) - 2); i++) {
+        sequence.push((sequence[sequence.length - 1] + sequence[sequence.length - 2]));
+    }
+    return sequence[sequence.length - 1];
 };
 
 // Do not edit below this line
