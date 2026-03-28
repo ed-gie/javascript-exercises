f6492ed3787c8811c78dd8c630524343bf67193a
Ed <ed.gie0696@gmail.com>
2026-03-28 14:58:55 +1300

    complete task and tests

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    return arr.reduce((oldest, obj) => {
+        if (!obj.yearOfDeath) {
+            obj.yearOfDeath = new Date().getFullYear();
+        }
+        if (!oldest.yearOfDeath) {
+            oldest.yearOfDeath = new Date().getFullYear();
+        }
+        if ((oldest.yearOfDeath - oldest.yearOfBirth) > (obj.yearOfDeath - obj.yearOfBirth)) {
+            return oldest;
+        }
+        else {
+            return obj;
+        }
+    }, arr[0])
 };
+
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1066,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
 
 // Do not edit below this line
 module.exports = findTheOldest;
