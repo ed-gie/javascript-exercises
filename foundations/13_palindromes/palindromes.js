e4c3a8eb6c030566e947b45650eee23bee863af6
Ed <ed.gie0696@gmail.com>
2026-03-28 13:15:42 +1300

    complete tasks and tests

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const validString = str
+        .split('')
+        .filter(char => char.match(/[a-zA-Z0-9]/))//removes unwanted characters
+        .join("")
+        .toLowerCase();
+    const reversed = validString
+        .split("")
+        .reverse()
+        .join("");
+    return validString === reversed;
 };
 
 // Do not edit below this line
