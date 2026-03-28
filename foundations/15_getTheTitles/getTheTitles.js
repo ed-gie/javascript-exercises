79ed06aa5cc7269e52415745be05c9e9889afc14
Ed <ed.gie0696@gmail.com>
2026-03-28 14:22:22 +1300

    complete task and test

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.map((obj) => obj.title);
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
 // Do not edit below this line
 module.exports = getTheTitles;
